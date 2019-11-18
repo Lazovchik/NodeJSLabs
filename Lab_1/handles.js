fcd13a58bcb1ade1a4bb13e2215a6b8bf9cb4a8a
lazovchik <lazovchik@gmail.com>
2019-11-18 18:42:08 +0100

    Update handles.js

--- Lab_1/handles.js
+++ Lab_1/handles.js
@@ -5,26 +5,27 @@ const qs = require('querystring')
 
 
 module.exports = {
-  serverHandle: function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname
-  const params = qs.parse(route.query)
+	serverHandle: function (req, res) {
+	const route = url.parse(req.url)
+	const path = route.pathname
+	const params = qs.parse(route.query)
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.writeHead(200, {'Content-Type': 'text/plain'});
 
-  if (path === '/hello' && 'name' in params) {
-	if(params['name']=='Artem')
-	 	res.write('Hello, my name is ' + params['name']+ ' I am a student is ECE Paris')
-	else
-    	res.write('Hello ' + params['name'])
-  } else {
+	if (path === '/hello' && 'name' in params) {
+		if(params['name']=='Artem')
+			res.write('Hello, my name is ' + params['name']+ ' I am a student is ECE Paris');
+		else
+			res.write('Hello ' + params['name']);
+	} 
+	else {
 		res.writeHead(404, {'Content-Type': 'text/plain'});
-   		res.write('Error 404')
-  }
+		res.write('Error 404');
+	}
 
-  res.end();
+		res.end();
 
-  }
+	}
 	/*
 	const AnotherFunction = function () {
 		return true
